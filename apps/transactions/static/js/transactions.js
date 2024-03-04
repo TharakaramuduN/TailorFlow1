09ba90f19db1d3de2b90d6f8bc1a2ce5869211c2
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-03-04 18:27:20 +0530

    added pagination in transactions app

---
+++ apps/transactions/static/js/transactions.js
@@ -0,0 +1,43 @@
+async function filterTransactions(page_num){
+    const searchQuery = document.querySelector('.search-input').value
+    let response = await fetch(`/filter-transactions/?search=${searchQuery}&page=${page_num ? page_num : 1}`)
+    if(!response.ok){
+        throw Error('not found')
+    }
+    const data = await response.json()
+    const transactionsContainer = document.querySelector('.transactions-list')
+    transactionsContainer.innerHTML = ''
+    pagination = document.querySelector('.pagination')
+    page_obj = data['page_obj']
+    data['transactions'].forEach(transaction=>{
+        const divElement = document.createElement('div')
+        divElement.classList.add('transaction-card', 'flex','justify-between', 'w-full','even:bg-slate-100', 'odd:bg-slate-50' ,'rounded-md', 'px-4', 'pb-3')
+        divElement.innerHTML = `
+        <div class="flex gap-2 sm:gap-4">
+            <div class="flex justify-center items-center"><img class=" h-11 w-11 rounded-full" src="/media/${transaction.order__customer__profile}" alt=""></div>
+            <div class="flex gap-1 flex-col">
+                <p class="text-lg font-medium">${transaction.order__customer__first_name}</p>
+                <p class=" text-sm  text-gray-500">Order Id:${transaction.order__id}</p>
+            </div>
+        </div>
+        <div class=" flex justify-center items-center font-bold"><p>${transaction.amount}</p></div>
+        `
+        transactionsContainer.appendChild(divElement)
+    })
+    pagination.innerHTML = ''
+    pagination.innerHTML = `
+    <span class="step-links flex justify-between w-full">
+        ${page_obj.has_previous ? `
+        <span>
+        <button class=" border px-3 bg-yellow-500 rounded-full" onclick=filterTransactions(${ page_obj.previous_page_number })><i class="fa-solid fa-angle-left"></i></button></span>` : '<div></div>' }
+
+        <span class="current">
+            Page ${ page_obj.number } of ${ page_obj.num_pages }.
+        </span>
+
+        ${page_obj.has_next ? `
+        <span>
+            <button class=' border px-3 bg-yellow-500 rounded-full' type='button' onclick=filterTransactions(${ page_obj.next_page_number })><i class="fa-solid fa-angle-right"></i></button>
+        </span>` : '<div></div>'}
+    </span>`
+}
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
 from .models import Transaction
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 # Create your views here.
 
 @login_required
 def transactions(request):
-    print(request.path)
     transactions = Transaction.objects.filter(tailor=request.user)
-    return render(request,'transactions/transactions.html',{'transactions':transactions})
+    paginator = Paginator(transactions,6)
+    page_obj = paginator.get_page(1)
+    return render(request,'transactions/transactions.html',{'page_obj':page_obj})
+
+@login_required
+def filter_transactions(request):
+    transactions = Transaction.objects.filter(tailor=request.user)
+    search_query = request.GET.get('search','')
+    if search_query:
+        transactions = transactions.filter(
+            Q(order__customer__first_name__icontains=search_query) |
+            Q(order__id__icontains=search_query)
+        )
+    paginator = Paginator(transactions,6)
+    page_num = request.GET.get('page')
+    print(page_num)
+    page_obj = paginator.get_page(page_num)
+    transactions_data = list(page_obj.object_list.values('order__id','amount', 'order__customer__first_name','order__customer__profile'))
+    print(transactions_data)
+    page_data = {
+        'number':page_obj.number,
+        'num_pages':page_obj.paginator.num_pages,
+        'has_next':page_obj.has_next(),
+        'has_previous':page_obj.has_previous(),
+        'previous_page_number':page_obj.previous_page_number() if page_obj.has_previous() else None,
+        'next_page_number':page_obj.next_page_number() if page_obj.has_next() else None,
+
+    }
+    return JsonResponse({'page_obj':page_data,'transactions':transactions_data})
