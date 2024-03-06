cc1c3f8a88b644818ed100aa144f2b17ce030b86
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-03-06 15:54:37 +0530

    customized default django password reset views

--- apps/tailors/urls.py
+++ apps/tailors/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('profile/',views.profile,name='profile'),
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name='tailors/password_reset.html'),name='password_reset'),
     path('password_reset_sent/',auth_views.PasswordResetDoneView.as_view(template_name='tailors/password_reset_sent.html'),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='tailors/set_password.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='tailors/password_reset_complete.html'), name='password_reset_complete'),
 ]
