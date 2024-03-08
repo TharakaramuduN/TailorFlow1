ada10c5bd46078c3379e6e087c4d97f2cdb3372f
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-03-08 17:31:30 +0530

    added default products and some styles

--- apps/products/static/js/products.js
+++ apps/products/static/js/products.js
@@ -11,22 +11,30 @@ async function filterProducts(){
     console.log(products)
     const productsContainer = document.querySelector('.products-container')
     productsContainer.innerHTML = ''
-    products['products'].forEach(product=>{
-        console.log(`/media/${product.image}`)
-        const divElement = document.createElement('div')
-        divElement.classList.add('product-card','rounded','shadow-lg','h-96','justify-center')
-        divElement.innerHTML = `
-            <div class="product-img h-[60%] sm:h-[70%]">
-                <img loading="lazy" class="w-full h-full sm:p-5 p-3 rounded-3xl" src="/media/${product.image }" alt="${product.title }">
-            </div>
-            <div class="px-6 py-4">
-                <div class="font-bold text-sm mb-2">${truncateWords(product.title,2)}</div>
-                <p class="text-gray-700 text-sm">${truncateWords(product.description,2)}</p>
-                <p class="text-gray-800 text-base font-semibold mt-2">₹${ product.price }</p>
-            </div>
-        `
+    const notFoundElement = document.querySelector('.not-found')
+    if(products['products'].length > 0){
+        if(notFoundElement.classList.contains('min-h-[50vh]')) notFoundElement.classList.remove('min-h-[50vh]')
+        notFoundElement.innerHTML = ''
+        products['products'].forEach(product=>{
+            console.log(`/media/${product.image}`)
+            const divElement = document.createElement('div')
+            divElement.classList.add('product-card','rounded','shadow-lg','h-96','justify-center')
+            divElement.innerHTML = `
+                <div class="product-img h-[60%] sm:h-[70%]">
+                    <img loading="lazy" class="w-full h-full sm:p-5 p-3 rounded-3xl" src="/media/${product.image }" alt="${product.title }">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-sm mb-2">${truncateWords(product.title,2)}</div>
+                    <p class="text-gray-700 text-sm">${truncateWords(product.description,2)}</p>
+                    <p class="text-gray-800 text-base font-semibold mt-2">₹${ product.price }</p>
+                </div>
+            `
         productsContainer.appendChild(divElement)
-    })
+    })}
+    else{
+        notFoundElement.classList.add('min-h-[50vh]')
+        notFoundElement.innerHTML = '<h1 class=" text-xl sm:text-3xl md:text-4xl lg:text-6xl  font-medium text-gray-500">No Results Found</h1>'
+    }
 }
 
 function truncateWords(text, limit) {
