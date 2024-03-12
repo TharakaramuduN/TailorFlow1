59cf772cb8f216f9f32c67a6aab17c8f7f675500
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-03-12 23:24:46 +0530

    added sales dashboard with filters

--- apps/dashboard/static/js/dashboard.js
+++ apps/dashboard/static/js/dashboard.js
@@ -1,10 +1,11 @@
-async function filterOrderItems(event){
-    const filterBtns = document.querySelectorAll('.filter-btn')
-    filterBy = event.target.id
-    filterBtns.forEach(btn=>{
-        if(btn.classList.contains('bg-gray-800')) btn.classList.remove('bg-gray-800','text-white')
+const filterBtns = document.querySelectorAll('.filter-btn')
+filterBtns.forEach(btn=>{
+    btn.addEventListener('click',(event)=>{
+        filterBtns.forEach(x=>x.classList.remove('bg-gray-800','text-white','selected-status'))
+        event.target.classList.add('bg-gray-800','text-white','selected-status')
     })
-    event.target.classList.add('bg-gray-800','text-white')
+})
+async function filterOrderItems(filterBy){
     let url;
     if(filterBy==='urgent'){
         url = `/filter-order-items/?urgent=true`
@@ -16,49 +17,66 @@ async function filterOrderItems(event){
         throw Error('Page not found')
     }
     const data = await response.json()
+    let itemsCountElement = document.querySelector(`.${filterBy}-count`)
+    itemsCountElement.innerText = data.order_items.length
     const productsContainerHeader = document.querySelector('.products-container-header')
     const productsContainer = document.querySelector('.products-container')
     productsContainer.innerHTML = ''
-    if(filterBy!=='Stitched'){
-        productsContainerHeader.innerHTML=`<div class="grid grid-cols-10  p-2 sm:px-3  bg-gray-800 text-white rounded-tr-md rounded-tl-md items-center text-sm sm:text-lg">
+    productsContainerHeader.innerHTML = ''
+    if(data.order_items.length > 0){
+        if(filterBy!=='Stitched'){
+            productsContainerHeader.innerHTML=`<div class="grid grid-cols-10  p-2 sm:px-3  bg-gray-800 text-white rounded-tr-md rounded-tl-md items-center text-sm sm:text-lg">
             <p class="  col-span-3">Name</p>
             <p class="col-span-4">Product Title</p>
             <p class="  col-span-2">Req. Date</p>
             <div></div>
-        </div>`
-        data.order_items.forEach((item) => {
-            const divElement = document.createElement('div')
-            divElement.classList.add('product-card','grid', 'grid-cols-10',  'p-2', 'sm:px-3' , 'even:bg-gray-200', 'odd:bg-gray-50', 'gap-2', 'text-sm', 'sm:text-base','items-center')
-            divElement.innerHTML = `
-                <p class="  col-span-3">${item.customer_name}</p>
-                <p class=" col-span-4">${truncateWords(item.title,2)}</p>
-                <p class="  col-span-2">${item.requested_date}</p>
-                <div id="${item.id}" class="check-btn-${item.id} hover:cursor-pointer"  onclick="updateStatus(event)" >
-                    <i id="${item.id}" class="fa-solid fa-check text-green-600"></i>
-                </div>
-            `
-            productsContainer.appendChild(divElement)
-        })}
-        else{
-            productsContainerHeader.innerHTML = `<div class="grid grid-cols-10  p-2 sm:px-3  bg-gray-800 text-white rounded-tr-md rounded-tl-md items-center text-sm sm:text-lg">
-                <p class="  col-span-4">Name</p>
-                <p class="col-span-4">Product Title</p>
-                <p class="  col-span-2">Stitched Date</p>
             </div>`
             data.order_items.forEach((item) => {
+                const dateStr = `${item.requested_date}`;
+                const date = new Date(dateStr);
+                const options = { month: 'short', day: 'numeric' };
+                const requested_date = date.toLocaleDateString('en-US', options);
                 const divElement = document.createElement('div')
                 divElement.classList.add('product-card','grid', 'grid-cols-10',  'p-2', 'sm:px-3' , 'even:bg-gray-200', 'odd:bg-gray-50', 'gap-2', 'text-sm', 'sm:text-base','items-center')
                 divElement.innerHTML = `
-                    <p class="  col-span-4">${item.customer_name}</p>
+                    <p class="  col-span-3">${item.customer_name}</p>
                     <p class=" col-span-4">${truncateWords(item.title,2)}</p>
-                    <p class="  col-span-2">${item.stitched_on}</p>
+                    <p class="  col-span-2">${requested_date}</p>
+                    <div id="${item.id}" class="check-btn-${item.id} hover:cursor-pointer"  onclick="updateStatus(event)" >
+                        <img class=" pointer-events-none  h-7 sm:h-7 w-7 sm:w-7" src="/static/icons/checked.png" alt="stitched">
+                    </div>
                 `
                 productsContainer.appendChild(divElement)
             })}
-            
+            else{
+                productsContainerHeader.innerHTML = `<div class="grid grid-cols-10  p-2 sm:px-3  bg-gray-800 text-white rounded-tr-md rounded-tl-md items-center text-sm sm:text-lg">
+                <p class="  col-span-4">Name</p>
+                <p class="col-span-4">Product Title</p>
+                <p class="  col-span-2">Stitched Date</p>
+                </div>`
+                data.order_items.forEach((item) => {
+                    const dateStr = `${item.stitched_on}`;
+                    const date = new Date(dateStr);
+                    const options = { month: 'short', day: 'numeric' };
+                    const stitched_date = date.toLocaleDateString('en-US', options);
+                    const divElement = document.createElement('div')
+                    divElement.classList.add('product-card','grid', 'grid-cols-10',  'p-2', 'sm:px-3' , 'even:bg-gray-200', 'odd:bg-gray-50', 'gap-2', 'text-sm', 'sm:text-base','items-center')
+                    divElement.innerHTML = `
+                        <p class="  col-span-4">${item.customer_name}</p>
+                        <p class=" col-span-4">${truncateWords(item.title,2)}</p>
+                        <p class="  col-span-2">${stitched_date}</p>
+                    `
+                    productsContainer.appendChild(divElement)
+                })}
+    }else{
+        productsContainer.innerHTML = `<div class="no-items h-full w-full flex items-center justify-center">
+                                            <h1 class=' text-3xl text-gray-600 font-medium'>No Items</h1>
+                                        </div>`
+    }
     }
 
 async function updateStatus(event){
+    const selectedStatusBtn = document.querySelector('.selected-status')
     const itemId = event.target.id
     const productsContainer = document.querySelector('.products-container')
     const productCard = event.target.closest('.product-card')
@@ -66,7 +84,16 @@ async function updateStatus(event){
     if(!response.ok){
         throw Error('Page Not found')
     }
-    productsContainer.removeChild(productCard)
+    if(selectedStatusBtn.id==='Not-Stitched'){
+        await filterOrderItems('Stitched')
+        await filterOrderItems('urgent')
+        await filterOrderItems('Not-Stitched')
+    }
+    else{
+        await filterOrderItems('Stitched')
+        await filterOrderItems('Not-Stitched')
+        await filterOrderItems('urgent')
+    }
 }
 
 function truncateWords(text, limit) {
@@ -76,4 +103,30 @@ function truncateWords(text, limit) {
     } else {
         return text;
     }
+}
+
+const selectElement = document.getElementById('filter-by-date')
+selectElement.addEventListener('change',filterSalesDashboard)
+
+async function filterSalesDashboard(){
+    filter_by_date = selectElement.value
+    url = `/filter-sales-dashboard/?filter_by_date=${filter_by_date}`
+    let response = await fetch(url)
+    if(!response.ok){
+        Error('not found')
+    }
+    const data = await response.json()
+    const totalOrdersElement = document.querySelector('.total-orders-count')
+    const totalCustomersElement = document.querySelector('.total-customers-count')
+    const totalSalesElement = document.querySelector('.total-sales-count')
+    const totalItemsElement = document.querySelector('.total-items-count')
+    const totalPrepaidElement = document.querySelector('.total-prepaid-count')
+    const totalPostpaidElement = document.querySelector('.total-postpaid-count')
+
+    totalOrdersElement.innerText = data.orders_count
+    totalCustomersElement.innerText = data.customers_count
+    totalSalesElement.innerText = '₹' + data.sales_count
+    totalItemsElement.innerText = data.items_count
+    totalPrepaidElement.innerText = data.prepaid_count
+    totalPostpaidElement.innerText = data.postpaid_count
 }
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -5,14 +5,30 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from apps.orders.forms import OrderForm
 from django.core.serializers import serialize
-from datetime import datetime
+from datetime import datetime,timedelta
+from apps.transactions.models import Transaction
+from apps.customers.models import Customer
 # Create your views here.
 
 @login_required
 def dashboard(request):
     orders = Order.objects.filter(tailor=request.user)
+    customers = Customer.objects.filter(tailor=request.user)
+    transactions = Transaction.objects.filter(tailor=request.user)
+    sales = sum([transaction.amount for transaction in transactions])
+    total_order_items = 0
+    delivered = 0
+    urgent = 0
+    stitched = 0
+    prepaid,postpaid = 0,0
+    prepaid += len(orders.filter(payment_type='PRE'))
+    postpaid += len(orders.filter(payment_type='POST'))
     order_items = []
     for order in orders:
+        total_order_items += len(order.items.all())
+        delivered += len(order.items.filter(status='Delivered'))
+        stitched += len(order.items.filter(status='Stitched'))
+        urgent += len(order.items.filter(urgent=True,status='Not-Stitched'))
         items = list(order.items.filter(status='Not-Stitched').order_by('requested_date'))    
         for item in items:
             order_items.append({
@@ -22,12 +38,16 @@ def dashboard(request):
                 'requested_date':item.requested_date,
             })
     order_items = sorted(order_items,key=lambda item:item['requested_date'])
-    return render(request,'dashboard/dashboard.html',{'order_items':order_items})
+    print(delivered)
+    return render(request,'dashboard/dashboard.html',{'order_items':order_items,'total_orders':len(orders),
+                                                      'sales':sales,'total_customers':len(customers),
+                                                      'delivered':delivered,'total_order_items':total_order_items,
+                                                      'urgent':urgent,'stitched':stitched,'prepaid':prepaid,'postpaid':postpaid})
 
 def update_status(request,item_id):
     order_item = OrderItem.objects.get(id=item_id)
     order_item.status = 'Stitched'
-    order_item.stitched_on = datetime.now()
+    order_item.stitched_on = datetime.now().date()
     order_item.save()
     return JsonResponse({'status':'success'})
 
@@ -53,3 +73,50 @@ def filter_order_items(request):
     if filter_by == 'Stitched':
         order_items = sorted(order_items,key=lambda item:item['stitched_on'],reverse=True)
     return JsonResponse({'order_items':order_items})
+
+def filter_sales_dashboard(request):
+    filter_by_date = request.GET.get('filter_by_date')
+    orders = Order.objects.filter(tailor=request.user)
+    customers = Customer.objects.filter(tailor=request.user)
+    transactions = Transaction.objects.filter(tailor=request.user)
+    if filter_by_date == 'Total':
+        orders = orders
+        customers = customers
+        transactions = transactions
+    if filter_by_date == 'Today':
+        orders = orders.filter(order_date_time__date = datetime.today())
+        customers = customers.filter(created_at__date = datetime.today())
+        transactions = transactions.filter(date_time__date=datetime.today())
+    elif filter_by_date == 'last_7_days':
+        delta = timedelta(days=7)
+        start_date = datetime.now() - delta
+        orders = orders.filter(order_date_time__gte = start_date)
+        customers = customers.filter(created_at__gte = start_date)
+        transactions = transactions.filter(date_time__gte = start_date)
+    elif filter_by_date == 'this_month':
+        month = datetime.now().month
+        year = datetime.now().year
+        orders = orders.filter(order_date_time__month = month,order_date_time__year=year)
+        customers = customers.filter(created_at__month = month,created_at__year=year)
+        transactions = transactions.filter(date_time__month=month,date_time__year=year)
+    elif filter_by_date == 'this_year':
+        year = datetime.now().year
+        orders = orders.filter(order_date_time__year=year)
+        customers = customers.filter(created_at__year=year)
+        transactions = transactions.filter(date_time__year=year)
+
+    orders_count = len(orders)
+    customers_count = len(customers)
+    sales_count = 0
+    items_count = 0
+    prepaid_count = len(orders.filter(payment_type='PRE'))
+    postpaid_count = len(orders.filter(payment_type='POST'))
+
+    for order in orders:
+        items_count += len(order.items.all())
+    
+    sales_count += sum([transaction.amount for transaction in transactions])
+
+    return JsonResponse({'orders_count':orders_count,'customers_count':customers_count,
+                         'sales_count':sales_count,'items_count':items_count,
+                         'prepaid_count':prepaid_count,'postpaid_count':postpaid_count})
